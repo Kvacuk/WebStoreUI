5f124db8d7542005c547c6e26ea050e7830dfca2
Bohdan Papirniak <bohdan_papirniak@epam.com>
2022-06-01 12:45:02 +0300

    Add node-modules

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const fs = require("fs");
+
+http.createServer(function (request, response) {
+    let filePath = "index.html";
+    if (request.url !== "/") {
+        filePath = request.url.substring(1);
+    }
+    fs.readFile(filePath, function (error, data) {
+        if (error) {
+            response.statusCode = 404;
+            response.end("Resoures Not Found");
+        }
+        else {
+            response.end(data);
+        }
+    });
+}).listen(3000, function () {
+    console.log("Server started at 3000");
+});
